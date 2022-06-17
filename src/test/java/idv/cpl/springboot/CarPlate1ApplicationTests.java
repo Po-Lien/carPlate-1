249a83753d58ccc69cbf68fc3fc8c0d2ee98bce5
Lien <polien1006@gmail.com>
2022-06-17 23:25:09 +0800

    #commit v0

---
+++ src/test/java/idv/cpl/springboot/CarPlate1ApplicationTests.java
@@ -0,0 +1,13 @@
+package idv.cpl.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPlate1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
