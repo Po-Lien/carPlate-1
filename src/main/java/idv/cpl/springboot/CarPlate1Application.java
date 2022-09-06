a3227ce3314cabd16e002dc302ab161f388d3dcc
Lien <polien1006@gmail.com>
2022-09-06 22:45:44 +0800

    test logback

--- src/main/java/idv/cpl/springboot/CarPlate1Application.java
+++ src/main/java/idv/cpl/springboot/CarPlate1Application.java
@@ -1,13 +1,30 @@
 package idv.cpl.springboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class CarPlate1Application {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+	private static final Logger log = LoggerFactory.getLogger(CarPlate1Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarPlate1Application.class, args);
+		log.debug(
+				"\n----------------------------------------------------------------------\n\t"
+						+ "Application '{}' is running!\n"
+						+ "----------------------------------------------------------------------",
+				CarPlate1Application.class.getSimpleName());
 	}
 
 }
--- src/test/java/idv/cpl/springboot/CarPlate1ApplicationTests.java
+++ src/test/java/idv/cpl/springboot/CarPlate1ApplicationTests.java
@@ -1,12 +1,8 @@
 package idv.cpl.springboot;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CarPlate1ApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
