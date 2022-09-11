fa9cb86194f4cb83a28f8ec58d3aab98e7d19a0f
Lien <polien1006@gmail.com>
2022-09-11 16:22:59 +0800

    add repository and Service file

---
+++ src/main/java/repository/DeptRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import idv.cpl.springboot.dto.DeptDTO;
+
+@Repository
+public interface DeptRepository extends JpaRepository<DeptDTO, Long> {
+
+}
